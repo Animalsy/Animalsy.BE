bab8db944e97b8e95255d20bf351f3deb2f63f4d
Prochownik, Dominik <d.prochownik17@gmail.com>
2024-06-02 16:57:40 +0200

    Add customer and vendor Id to the default IdentityUser

--- Animalsy.BE.Services.AuthAPI/Data/AppDbContext.cs
+++ Animalsy.BE.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,10 +1,10 @@
-﻿
-using Microsoft.AspNetCore.Identity;
+﻿using Animalsy.BE.Services.AuthAPI.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Animalsy.BE.Services.AuthAPI.Data;
 
-public class AppDbContext(DbContextOptions options) : IdentityDbContext<IdentityUser>(options)
+public class AppDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 }
---
+++ Animalsy.BE.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Animalsy.BE.Services.AuthAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public Guid CustomerId { get; set; }
+        public Guid VendorId { get; set; }
+    }
+}
