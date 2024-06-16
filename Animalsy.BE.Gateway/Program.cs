fe3d81e6cf439026c5d83ae3669941bc19c4158e
Prochownik, Dominik <d.prochownik17@gmail.com>
2024-06-16 19:14:32 +0200

    Configure gateway routes

--- Animalsy.BE.Gateway/Program.cs
+++ Animalsy.BE.Gateway/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.AddAppAuthentication();
 
 var app = builder.Build();
